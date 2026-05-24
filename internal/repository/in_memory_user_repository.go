24cb3ac9552970ded56a418541884fa032e57d05
Wide97 <mwidesott@gmail.com>
2026-05-24 20:09:43 +0200

    feat: ended in memory user repo

--- internal/repository/in_memory_user_repository.go
+++ internal/repository/in_memory_user_repository.go
@@ -49,3 +49,47 @@ func (r *InMemoryUserRepository) GetByID(id int) (*model.User, error) {
 	return &u, nil
 
 }
+
+func (r *InMemoryUserRepository) GetAll() ([]model.User, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	users := make([]model.User, 0, len(r.users))
+	for _, u := range r.users {
+		users = append(users, u)
+	}
+
+	return users, nil
+
+}
+
+func (r *InMemoryUserRepository) Update(user model.User) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	_, ok := r.users[user.ID]
+	if !ok {
+		return errors.ErrUserNotFound
+	}
+
+	user.UpdatedAt = time.Now()
+
+	r.users[user.ID] = user
+
+	return nil
+
+}
+
+func (r *InMemoryUserRepository) Delete(id int) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	_, ok := r.users[id]
+	if !ok {
+		return errors.ErrUserNotFound
+	}
+
+	delete(r.users, id)
+
+	return nil
+}
