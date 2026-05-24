2de0094e20fac4e7dc7ce4a3392bc6605ee34b10
Wide97 <mwidesott@gmail.com>
2026-05-24 19:43:12 +0200

    feat: added Create & GetById func

--- internal/repository/in_memory_user_repository.go
+++ internal/repository/in_memory_user_repository.go
@@ -2,7 +2,9 @@ package repository
 
 import (
 	"sync"
+	"time"
 
+	"github.com/mwidesott/go-user-service/internal/errors"
 	"github.com/mwidesott/go-user-service/internal/model"
 )
 
@@ -18,4 +20,32 @@ func NewInMemoryUserRepository() *InMemoryUserRepository {
 	}
 
 	return r
+}
+
+func (r *InMemoryUserRepository) Create(u model.User) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.counterId++
+	u.ID = r.counterId
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = time.Now()
+
+	r.users[u.ID] = u
+
+	return nil
+
+}
+
+func (r *InMemoryUserRepository) GetByID(id int) (*model.User, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	u, ok := r.users[id]
+	if !ok {
+		return nil, errors.ErrUserNotFound
+	}
+
+	return &u, nil
+
 }
